feacbb84dbab51dc63e6b31a33b954b7a3587cfb
Robert <meel4robert@hotmail.comm>
2019-01-24 14:45:14 +0100

    removed :

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo '<html><head>OK</head><body>';
 echo '<p/>if you can read this, OKD is UP!';
-echo "<br/>regards, : ";
+echo "<br/>regards, ";
 echo gethostname();
 echo "</body></html>";
 ?>
