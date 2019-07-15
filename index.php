c0fcf570032f98fd1b1087dff3000a4eec78ddf0
Robert <meel4robert@hotmail.comm>
2019-07-15 19:27:47 +0200

    demo

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo '<html><head>OK</head><body>';
-echo '<p/>if you can read this, OKD is UP!';
+echo '<p/>if you can read this, The Terra10 OCP is UP!';
 echo "<br/>regards, ";
 echo gethostname();
 echo "</body></html>";
